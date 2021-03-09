7a425955faf1412c33219745adddf5a26feeca8b
Allan Jorge <jobs@allanj.org>
2021-03-09 21:20:21 +0100

    vehicle: add function stubs

---
+++ src/vehicle.service.ts
@@ -0,0 +1,9 @@
+import { VehicleInterface } from './vehicle.interface'
+
+export function calculateAverageMileage (vehicle: VehicleInterface) {
+  throw new Error('Not implemented')
+}
+
+export function estimateCurrentMileage (vehicle: VehicleInterface) {
+  throw new Error('Not implemented')
+}
