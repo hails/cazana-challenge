4837835f758b7541bfe098a127f0be097f1441c8
Allan Jorge <jobs@allanj.org>
2021-03-10 16:44:09 +0100

    vehicle: add calculateAverageMileage complementary tests

--- test/vehicle.test.ts
+++ test/vehicle.test.ts
@@ -40,3 +40,78 @@ test('should calculate the average annual mileage', () => {
 
   expect(calculateAverageMileage(vehicle)).toBe(5000)
 })
+
+test('should calculate the average annual mileage with long dated events', () => {
+  const adsForSaleEvent = {
+    date: new Date('2000'),
+    price: 1000000,
+    mileage: 1500
+  } as AdsForSaleEventInterface
+
+  const motTestEvent = {
+    date: new Date('2015'),
+    mileage: 50000,
+    result: 'pass'
+  } as MOTTestEventInterface
+
+  const changeOfVRMEvent = {
+    date: new Date('2020'),
+    from: 'IZVB51',
+    to: 'IZVB52'
+  } as ChangeOfVRMEventInterface
+
+  const vehicle: VehicleInterface = {
+    id: 'foobar',
+    VRM: 'IZVB52',
+    maker: 'Ford',
+    model: 'Fiesta',
+    registrationDate: new Date('1999'),
+    events: [
+      adsForSaleEvent,
+      motTestEvent,
+      changeOfVRMEvent
+    ]
+  }
+
+  const caculatedAverageMileage = calculateAverageMileage(vehicle)
+
+  expect(Math.round(caculatedAverageMileage)).toBe(3029)
+})
+
+test('should return the average annual mileage as 7900 when vehicle has no events', () => {
+  const vehicle: VehicleInterface = {
+    id: 'foobar',
+    VRM: 'IZVB52',
+    maker: 'Ford',
+    model: 'Fiesta',
+    registrationDate: new Date('2020'),
+    events: [
+
+    ]
+  }
+
+  const caculatedAverageMileage = calculateAverageMileage(vehicle)
+
+  expect(Math.round(caculatedAverageMileage)).toBe(7900)
+})
+
+test('should return the average annual mileage as 7900 when vehicle has no events with mileage info', () => {
+  const changeOfVRMEvent = {
+    date: new Date('2020'),
+    from: 'IZVB51',
+    to: 'IZVB52'
+  } as ChangeOfVRMEventInterface
+
+  const vehicle: VehicleInterface = {
+    id: 'foobar',
+    VRM: 'IZVB52',
+    maker: 'Ford',
+    model: 'Fiesta',
+    registrationDate: new Date('1999'),
+    events: [changeOfVRMEvent]
+  }
+
+  const caculatedAverageMileage = calculateAverageMileage(vehicle)
+
+  expect(Math.round(caculatedAverageMileage)).toBe(7900)
+})
